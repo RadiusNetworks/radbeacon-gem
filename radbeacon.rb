9eb72db3baaef7b38326b89313bcc207fda7e1f1
James Nebeker <jnebeker@radiusnetworks.com>
2015-03-12 16:23:43 -0400

    WIP scan for connectable radbeacons

--- radbeacon.rb
+++ radbeacon.rb
@@ -12,12 +12,23 @@ class LeScanner
     scan_output.each_line do |line|
       result = line.scan(/^([A-F0-9:]{15}[A-F0-9]{2}) (.*)$/)
       if !result.empty?
-        mac_addr = result[0][0]
-        name = result[0][1]
-        devices << result[0]
+        device = BluetoothLeDevice.new(result[0][0], result[0][1])
+        devices << device
       end
     end
     devices
+  end
+
+  def radbeacon_scan(duration)
+    connectable_devices = Array.new
+    devices = self.scan(duration)
+    devices.each do |device|
+      if device.can_connect?
+        device.is_connectable = true
+        connectable_devices << device
+      end
+    end
+    connectable_devices
   end
 
 end
@@ -73,13 +84,16 @@ class BluetoothLeDevice
   ADVERTISING_RATE_VALUES = {"2" => "E002", "4" => "5001", "6" => "E000", "8" => "9000", "10" => "6000",
     "12" => "5000", "14" => "3500", "16" => "3000", "18" => "2500", "20" => "2000"}
 
-
-  def initialize(mac_address)
+  attr_accessor :mac_address, :name, :is_connectable, :is_radbeacon
+  def initialize(mac_address, name)
     @mac_address = mac_address
+    @name = name
+    @is_connectable = false
+    @is_radbeacon = false
   end
 
   def display
-    puts "MAC Address: " + @mac_address
+    puts "MAC Address: " + @mac_address + " Name: " + @name + " Can connect: " + @is_connectable.to_s
   end
 
   def update_params(beacon)
@@ -139,6 +153,20 @@ class BluetoothLeDevice
     result
   end
 
+  def can_connect?
+    result = false
+    timeout = 0.5
+    cmd = "gatttool -b #{@mac_address} --interactive"
+    PTY.spawn(cmd) do |output, input, pid|
+      output.expect(/\[LE\]>/)
+      input.puts "connect"
+      if output.expect(/Connection successful/, timeout)
+        result = true
+      end
+      input.puts "quit"
+    end
+    result
+  end
 end
 
 class Radbeacon
@@ -219,18 +247,14 @@ class Radbeacon
 end
 
 if __FILE__ == $0
-  my_beacon = Radbeacon.new('James\'s Test Beacon', '842AF9C4-08F5-11E3-9282-F23C91AEC05E', '707', '707', '-66', '0x0f', '0x8000', '0000', '1234')
-  my_other_beacon = Radbeacon.new('James\'s Test Beacon', '842AF9C4-08F5-11E3-9282-F23C91AEC05E', '707', '707', '-66', '0x0f', '0x8000', '1234', '0000')
-  #my_beacon.display
-
-  my_ble_device = BluetoothLeDevice.new('00:07:80:15:74:5B')
-  #my_ble_device.display
-  #do_actions
-
-  devices = LeScanner.new.scan(2)
-  puts devices.inspect
-
   def do_actions
+    my_beacon = Radbeacon.new('James\'s Test Beacon', '842AF9C4-08F5-11E3-9282-F23C91AEC05E', '707', '707', '-66', '0x0f', '0x8000', '0000', '1234')
+    my_other_beacon = Radbeacon.new('James\'s Test Beacon', '842AF9C4-08F5-11E3-9282-F23C91AEC05E', '707', '707', '-66', '0x0f', '0x8000', '1234', '0000')
+    #my_beacon.display
+
+    my_ble_device = BluetoothLeDevice.new('00:07:80:15:74:5B', 'RadBeacon USB')
+    my_ble_device.display
+
     puts "Updating PIN..."
     if my_ble_device.update_pin(my_beacon)
       puts "Update PIN Success!"
@@ -252,4 +276,16 @@ if __FILE__ == $0
       puts "Lock Success!"
     end
   end
+
+  #do_actions
+  devices = LeScanner.new.scan(1)
+  puts "Discovered " + devices.count.to_s + " BLE devices"
+  connectable_devices = LeScanner.new.radbeacon_scan(1)
+  puts "Discovered " + connectable_devices.count.to_s + " connectable BLE devices"
+
+  #puts devices.inspect
+
+  #my_ble_device = BluetoothLeDevice.new('00:07:80:15:74:5B', 'RadBeacon USB')
+  #puts "My BLE device can connect? --> " + my_ble_device.is_connectable.to_s
+
 end
