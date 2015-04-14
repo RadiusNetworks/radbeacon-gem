e39e658e274fe27ecf6940ebbc79783e50c95d9e
James Nebeker <jnebeker@radiusnetworks.com>
2015-04-14 18:06:35 +0000

    fix le_scanner tests

--- spec/le_scanner_spec.rb
+++ spec/le_scanner_spec.rb
@@ -2,13 +2,13 @@ require 'le_scanner'
 
 describe LeScanner do
   describe '#scan' do
-    it "scans for beacons"
-      scanner = LeScanner.new(5)
-      devices = scanner.scan
+    scanner = LeScanner.new(5)
+    devices = scanner.scan
+    it "scans for beacons" do
       expect(devices.length).to be > 0
     end
 
-    it "filters out duplicate beacons in one scan"
+    it "filters out duplicate beacons in one scan" do 
       expect(devices.uniq.length).to eq devices.length
     end
   end
