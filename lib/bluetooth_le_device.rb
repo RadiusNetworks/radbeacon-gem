d30c0ba14fe2905b4855f22dc7cce46217776123
James Nebeker <jnebeker@radiusnetworks.com>
2015-04-16 17:13:38 -0400

    Add errors array and protect attributes from being written

--- lib/bluetooth_le_device.rb
+++ lib/bluetooth_le_device.rb
@@ -2,9 +2,12 @@ require 'pty'
 require 'expect'
 
 class BluetoothLeDevice
-  attr_accessor :mac_address, :name, :is_connectable, :characteristics, :values
+  attr_reader :mac_address, :name, :is_connectable, :characteristics, :values, :errors
+
+  TIMEOUT = 0.5
 
   def initialize(mac_address, name)
+    self.errors = []
     self.mac_address = mac_address
     self.name = name
     self.is_connectable = false
@@ -17,23 +20,30 @@ class BluetoothLeDevice
   end
 
   def fetch_characteristics
+    result = false
     if self.can_connect?
       self.is_connectable = true
-      self.discover_characteristics
-      self.char_values
+      if self.discover_characteristics
+        if self.char_values
+          result = true
+        end
+      end
     end
+    result
   end
-  
+
   def can_connect?
+    @errors = []
     result = false
-    timeout = 0.5
     cmd = "gatttool -b #{self.mac_address} --interactive"
     PTY.spawn(cmd) do |output, input, pid|
       output.expect(/\[LE\]>/)
       input.puts "connect"
-      if output.expect(/Connection successful/, timeout)
+      if output.expect(/Connection successful/, TIMEOUT)
         self.is_connectable = true
         result = true
+      else
+        @errors << "Connection failed"
       end
       input.puts "quit"
     end
@@ -41,35 +51,57 @@ class BluetoothLeDevice
   end
 
   def discover_characteristics
+    @errors = []
+    result = false
     output = ""
-    cmd = "gatttool -b #{self.mac_address} --characteristics"
+    cmd = "gatttool -b #{self.mac_address} --characteristics 2>&1"
     output = `#{cmd}`
-    output.each_line do |line|
-      result = line.scan(/^handle = (0x[a-f0-9]{4}), char properties = (0x[a-f0-9]{2}), char value handle = (0x[a-f0-9]{4}), uuid = ([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})$/)
-      if !result.empty?
-        characteristic = {"handle" => result[0][0], "properties" => result[0][1], "value_handle" => result[0][2], "uuid" => result[0][3]}
-        self.characteristics << characteristic
+    if output.strip != "Discover all characteristics failed: Internal application error: I/O"
+      self.characteristics = []
+      output.each_line do |line|
+        result = line.scan(/^handle = (0x[a-f0-9]{4}), char properties = (0x[a-f0-9]{2}), char value handle = (0x[a-f0-9]{4}), uuid = ([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})$/)
+        if !result.empty?
+          characteristic = {"handle" => result[0][0], "properties" => result[0][1], "value_handle" => result[0][2], "uuid" => result[0][3]}
+          self.characteristics << characteristic
+        end
       end
+      result = true
+    else
+      @errors << "Discover characteristics failed"
     end
-    self.characteristics
+    result
   end
 
   def char_values
-    timeout = 0.5
+    @errors = []
+    result = false
     cmd = "gatttool -b #{self.mac_address} --interactive"
-    PTY.spawn(cmd) do |output, input, pid|
-      output.expect(/\[LE\]>/)
-      input.puts "connect"
-      if output.expect(/Connection successful/, timeout)
-         self.characteristics.each do |char|
+    if self.characteristics != []
+      PTY.spawn(cmd) do |output, input, pid|
+        output.expect(/\[LE\]>/)
+        input.puts "connect"
+        if output.expect(/Connection successful/, TIMEOUT)
+          self.characteristics.each do |char|
             input.puts "char-read-hnd #{char['value_handle']}"
-            if output.expect(/Characteristic value\/descriptor: /, timeout)
-              value = output.expect(/^[0-9a-f\s]+\n/, timeout)
+            if output.expect(/Characteristic value\/descriptor: /, TIMEOUT)
+              value = output.expect(/^[0-9a-f\s]+\n/, TIMEOUT)
               self.values[char['value_handle']] = value.first.strip
             end
-         end
+          end
+          result = true
+        else
+          @errors << "Fetch characteristic values failed"
+        end
+        input.puts "quit"
       end
-      input.puts "quit"
+    else
+      @errors << "No characteristics present"
     end
+    result
   end
+
+  private
+
+  attr_writer :mac_address, :name, :is_connectable, :characteristics, :values, :errors
+
 end
