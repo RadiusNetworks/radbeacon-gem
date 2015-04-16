14d2a10057a01718e61cf1fe21d75dfee63b9888
James Nebeker <jnebeker@radiusnetworks.com>
2015-04-16 17:18:37 -0400

    Add validate method, add defaults setter, add proper power/advertising values
    The validate method validates all parameters of the radbeacon before trying to send an update to the beacon.

--- lib/radbeacon_usb.rb
+++ lib/radbeacon_usb.rb
@@ -5,13 +5,13 @@ class RadbeaconUsb < BluetoothLeDevice
 
   ## Define GATT characteristic constants
   # Generic Access Profile
-  C_DEVICE_NAME = '0x0003'
-  C_APPEARANCE  = '0x0006'
+  C_DEVICE_NAME = "0x0003"
+  C_APPEARANCE  = "0x0006"
   # Device Information
-  C_MANUFACTURER_NAME = '0x000a'
-  C_MODEL_NUMBER      = '0x000d'
-  C_SERIAL_STRING     = '0x0010'
-  C_FIRMWARE_STRING   = '0x0013'
+  C_MANUFACTURER_NAME = "0x000a"
+  C_MODEL_NUMBER      = "0x000d"
+  C_SERIAL_STRING     = "0x0010"
+  C_FIRMWARE_STRING   = "0x0013"
   # Configuration
   GATT_DEV_MODEL     = "0x0017"
   GATT_DEV_ID        = "0x001a"
@@ -46,14 +46,18 @@ class RadbeaconUsb < BluetoothLeDevice
   GATT_ERROR       = "00000002" #not used
 
   ## Transmit power and advertisement frequency values
-  TRANSMIT_POWER_VALUES = {"-23" => "00", "-21" => "01", "-18" => "03", "-14" => "05",
-    "-11" => "07", "-7" => "09", "-4" => "0b", "+0" => "0d", "+3" => "0f"}
-  MEASURED_POWER_VALUES = ["-94", "-92", "-90", "-86", "-84", "-79", "-74", "-72", "-66"]
-  ADVERTISING_RATE_VALUES = {"2" => "E002", "4" => "5001", "6" => "E000", "8" => "9000", "10" => "6000",
-    "12" => "5000", "14" => "3500", "16" => "3000", "18" => "2500", "20" => "2000"}
+  TRANSMIT_POWER_VALUES = {-23 => "00", -21 => "01", -18 => "03", -14 => "05",
+    -11 => "07", -7 => "09", -4 => "0b", 0 => "0d", 3 => "0f"}
+  DEFAULT_MEASURED_POWER_VALUES = [-94, -92, -90, -86, -84, -79, -74, -72, -66]
+  ADVERTISING_RATE_VALUES = {0 => "0000", 1 => "2006", 2 => "0003", 3 => "f501", 4 => "7001",
+    5 => "2001", 6 => "ea00", 7 => "c400", 8 => "a800", 9 => "9100", 10 => "8000"}
+  BEACON_TYPES = {"ibeacon" => "01", "altbeacon" => "02", "dual" => "03"}
+
+  ## Timeout length for GATT commands
+  TIMEOUT = 0.5
 
   attr_reader :mac_address, :errors, :dev_model, :dev_id, :dev_version
-  attr_accessor :dev_name, :uuid, :major, :minor, :power, :tx_power, :adv_interval
+  attr_accessor :dev_name, :uuid, :major, :minor, :power, :tx_power, :adv_rate, :beacon_type
 
   def initialize(device)
     self.errors = []
@@ -66,76 +70,109 @@ class RadbeaconUsb < BluetoothLeDevice
     self.major =  RadbeaconUtils.bytes_to_major_minor(device.values[GATT_MAJOR])
     self.minor = RadbeaconUtils.bytes_to_major_minor(device.values[GATT_MINOR])
     self.power = RadbeaconUtils.bytes_to_power(device.values[GATT_POWER])
-    self.tx_power = device.values[GATT_TXPOWER]
-    self.adv_interval = device.values[GATT_INTERVAL]
+    self.tx_power = TRANSMIT_POWER_VALUES.key(device.values[GATT_TXPOWER])
+    self.adv_rate = ADVERTISING_RATE_VALUES.key(device.values[GATT_INTERVAL].delete(' '))
+    self.beacon_type = BEACON_TYPES.key(device.values[GATT_BCTYPE])
   end
 
   def valid?
-
+    @errors = []
+    checks = {}
+    checks['device name'] = self.dev_name.length <= 20
+    checks['UUID'] = self.uuid.match(/^[A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{12}$/)
+    checks['major value'] = self.major.to_i.between?(0, 65535)
+    checks['minor value'] = self.minor.to_i.between?(0, 65535)
+    checks['measured power value'] = self.power.to_i.between?(-127, -1)
+    checks['transmit power'] = TRANSMIT_POWER_VALUES.has_key?(self.tx_power)
+    checks['advertising rate'] = ADVERTISING_RATE_VALUES.has_key?(self.adv_rate)
+    checks['beacon type'] = BEACON_TYPES.has_key?(self.beacon_type)
+    failed_checks = checks.select{|key, value| !value}
+    if failed_checks == {}
+      result = true
+    else
+      failed_checks.each do |key, value|
+        @errors << "Invalid #{key}"
+      end
+      result = false
+    end
+    result
   end
 
   def save!(pin)
-    @errors = []
-    update_params_commands = ["#{GATT_DEV_NAME} #{RadbeaconUtils.name_to_bytes(self.dev_name)}",
-      "#{GATT_UUID} #{RadbeaconUtils.uuid_to_bytes(self.uuid)}", "#{GATT_MAJOR} #{RadbeaconUtils.major_minor_to_bytes(self.major)}",
-      "#{GATT_MINOR} #{RadbeaconUtils.major_minor_to_bytes(self.minor)}", "#{GATT_POWER} #{RadbeaconUtils.power_to_bytes(self.power)}", "#{GATT_TXPOWER} 0f",
-      "#{GATT_INTERVAL} 8000", "#{GATT_ACTION} #{GATT_ACTION_UPDATE_ADV}", "#{GATT_PIN} #{RadbeaconUtils.pin_to_bytes(pin)}"]
+    if self.valid?
+      update_params_commands = ["#{GATT_DEV_NAME} #{RadbeaconUtils.text_to_bytes(self.dev_name)}",
+        "#{GATT_UUID} #{RadbeaconUtils.uuid_to_bytes(self.uuid)}", "#{GATT_MAJOR} #{RadbeaconUtils.major_minor_to_bytes(self.major)}",
+        "#{GATT_MINOR} #{RadbeaconUtils.major_minor_to_bytes(self.minor)}", "#{GATT_POWER} #{RadbeaconUtils.power_to_bytes(self.power)}",
+        "#{GATT_TXPOWER} #{TRANSMIT_POWER_VALUES[self.tx_power]}", "#{GATT_INTERVAL} #{ADVERTISING_RATE_VALUES[self.adv_rate]}",
+        "#{GATT_BCTYPE} #{BEACON_TYPES[self.beacon_type]}", "#{GATT_ACTION} #{GATT_ACTION_UPDATE_ADV}", "#{GATT_PIN} #{RadbeaconUtils.pin_to_bytes(pin)}"]
       result = con(update_params_commands)
+    else
+      result = false
+    end
+    result
   end
 
-  def change_pin(new_pin, old_pin)
-    @errors = []
+  def change_pin!(new_pin, old_pin)
     update_pin_commands = ["#{GATT_NEW_PIN} #{RadbeaconUtils.pin_to_bytes(new_pin)}", "#{GATT_ACTION} #{GATT_ACTION_UPDATE_PIN}",
       "#{GATT_PIN} #{RadbeaconUtils.pin_to_bytes(old_pin)}"]
     result = con(update_pin_commands)
   end
 
-  def factory_reset(pin)
-    @errors = []
+  def factory_reset!(pin)
     reset_commands = ["#{GATT_ACTION} #{GATT_ACTION_FACTORY_RESET}", "#{GATT_PIN} #{RadbeaconUtils.pin_to_bytes(pin)}"]
     result = con(reset_commands)
-    #self.fetch_params
+    if result
+      defaults
+    end
     result
   end
 
-  def boot_to_dfu(pin)
-    @errors = []
+  def boot_to_dfu!(pin)
     dfu_commands = ["#{GATT_ACTION} #{GATT_ACTION_DFU}", "#{GATT_PIN} #{RadbeaconUtils.pin_to_bytes(pin)}"]
     result = con(dfu_commands)
   end
 
-  def lock(pin)
-    @errors = []
+  def lock!(pin)
     lock_commands = ["#{GATT_ACTION} #{GATT_ACTION_LOCK}", "#{GATT_PIN} #{RadbeaconUtils.pin_to_bytes(pin)}"]
     result = con(lock_commands)
   end
 
   private
-
   attr_writer :mac_address, :errors, :dev_model, :dev_id, :dev_version
 
+  def defaults
+    self.dev_name = "RadBeacon USB"
+    self.uuid = "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6"
+    self.major = 1
+    self.minor = 1
+    self.power = -66
+    self.tx_power = 3
+    self.adv_rate = 10
+    self.beacon_type = "dual"
+  end
+
   def con(commands)
+    @errors = []
     result = false
-    timeout = 0.5
     cmd = "gatttool -b #{self.mac_address} --interactive"
     PTY.spawn(cmd) do |output, input, pid|
       output.expect(/\[LE\]>/)
       input.puts "connect"
-      if output.expect(/Connection successful/, timeout)
+      if output.expect(/Connection successful/, TIMEOUT)
         commands.each do |cmd|
           cmd = "char-write-req #{cmd}"
           input.puts cmd
-          if output.expect(/Characteristic value was written successfully/, timeout)
+          if output.expect(/Characteristic value was written successfully/, TIMEOUT)
             result = true
           else
-            @errors << "Write parameters failed: #{cmd}"
+            @errors << "Action failed: #{cmd}"
             result = false
             break
           end
         end
         if result
           input.puts "char-read-hnd #{GATT_RESULT}"
-          if output.expect(/Characteristic value\/descriptor: 00 00 00 00/, timeout)
+          if output.expect(/Characteristic value\/descriptor: 00 00 00 00/, TIMEOUT)
             result = true
           else
             @errors << "Invalid PIN"
