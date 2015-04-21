d75ac5b7d76692d3a12e13bbebacde2625d73167
James Nebeker <jnebeker@radiusnetworks.com>
2015-04-21 10:16:24 -0400

    Reorder require statements so Utils is before Usb

--- lib/radbeacon.rb
+++ lib/radbeacon.rb
@@ -1,10 +1,9 @@
 require "radbeacon/version"
 require "radbeacon/le_scanner"
 require "radbeacon/bluetooth_le_device"
+require "radbeacon/utils"
 require "radbeacon/scanner"
 require "radbeacon/usb"
-require "radbeacon/utils"
-
 module Radbeacon
 
 end
